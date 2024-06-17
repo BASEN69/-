6de9bade90bb49d19f5ba6b919d01413aaa32333
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-06-17 06:32:21 +0000

    nameの追加・除去

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_17_054038) do
+ActiveRecord::Schema.define(version: 2024_06_17_062745) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
